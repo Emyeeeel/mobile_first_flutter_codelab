e7d116e1e93b70d0e9397919f15d01caa85fb9e2
Emyeeeel <maryamielcatado@gmail.com>
2023-09-10 21:03:20 +0800

    add a home state and app notifier

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../state/my_app_state.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var wordPair = appState.current;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('A random idea'),
+            Text(wordPair.asPascalCase),
+            ElevatedButton(
+              onPressed: () => appState.generateWordPair(),
+              child: const Text('Next'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/state/my_app_state.dart
@@ -0,0 +1,11 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/widgets.dart';
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void generateWordPair() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
